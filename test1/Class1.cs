b498cb9fc75160102d607153f7f26910df1d2b34
zhang dong <zhangdong945@hotmail.com>
2018-09-16 23:08:44 +0800

    add class program

---
+++ test1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Program
+    {
+        void Main()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
